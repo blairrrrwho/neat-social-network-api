4a6164e178a9d994c640a3b7189b2b53f03ee3e8
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-05-07 19:51:04 -0500

    added comments for routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -9,14 +9,17 @@ const {
   deleteFriend
 } = require('../../controllers/thoughtController.js');
 
+// /api/users/
 router.route('/').get(getUsers).post(createUser);
 
+// /api/users/:userId
 router
   .route('/:id')
   .get(getUserById)
   .put(updateUser)
   .delete(deleteUser);
 
+// /api/users/:userId/friends/:friendId
 router
   .route('/:id/friends/:friendId')
   .post(addFriend)
